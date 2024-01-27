ad209670528c5f47f5db854f58ebb6de8760306b
Eric Rodrigues <128868563+EricWARodrigues@users.noreply.github.com>
2024-01-27 19:58:33 -0300

    Add files via upload

---
+++ Flask/02_aula/Calculadora_IMC/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def somar():
+    data: dict = request.form
+    height = float(data.get('height')) # "name" do Input no HTML
+    weight = float(data.get('weight')) # "name" do Input no HTML
+    print(height)
+    print(weight)
+
+    imc = ( weight / ( height ** 2 ))
+    categoria = ''
+
+    if imc < 18.5:
+        categoria = 'Abaixo do peso'
+    elif imc < 25:
+        categoria = 'Peso normal'
+    elif imc < 30:
+        categoria = 'Sobre Peso'
+    elif imc < 35:
+        categoria = 'Obesidade Grau 1'
+    elif imc < 40:
+        categoria = 'Obesidade Grau 2'
+    else:
+        categoria = 'Obesidade Grau 3'
+    
+    imc = str(round(imc, 2))
+    
+    dicionario_valores = {
+        'imc': imc,
+        'categoria': categoria
+    }
+    
+
+    # Passando a variavel "soma" para ser renderizada pelo Jinja
+    return render_template('index.html', imc = imc, categoria=categoria)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Flask/02_aula/Soma/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def somar():
+    data: dict = request.form
+    numero1 = data.get('num-1') # "name" do Input no HTML
+    numero2 = data.get('num-2') # "name" do Input no HTML 
+    
+    # Convertendo para Float
+    if numero1.strip() != '':
+        numero1 = float(numero1)
+    else:
+        numero1 = 0
+
+    # Convertendo para Float
+    if numero2.strip() != '':
+        numero2 = float(numero2)
+    else:
+        numero2 = 0
+    
+    resultado = numero1 + numero2
+
+    # Passando a variavel "soma" para ser renderizada pelo Jinja
+    return render_template('index.html', soma=resultado)
+
+if __name__ == '__main__':
+    app.run(debug=True)
